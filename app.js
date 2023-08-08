6f384ad5782795061a2e4acf0edad6945f90edfa
Holt Moore <holt.w.moore@gmail.com>
2023-08-08 13:35:42 -0500

    fifth

--- app.js
+++ app.js
@@ -43,80 +43,107 @@ db.on('open', () => {
     });
 });
 // Querying by comparison
-Vampire.find({ gender: 'f' }, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
-
-Vampire.find({ victims: { $gt: 500 } }, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
-
-Vampire.find({ victims: { $lte: 150 } }, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
-
-Vampire.find({ victims: { $ne: 210234 } }, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
-
-Vampire.find({ 
-    victims: { $gt: 150, $lt: 500 } 
-}, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
-
-Vampire.find({ 
-    victims: { $gt: 150, $lt: 500 } 
-}, (err, vampires) => {
-    if (err) console.log(err);
-    console.log(vampires);
-});
+async function fetchVampiresByComparison() {
+    try {
+      const femaleVampires = await Vampire.find({ gender: 'f' });
+      console.log(femaleVampires);
+  
+      const vampiresWithMoreThan500Victims = await Vampire.find({ victims: { $gt: 500 } });
+      console.log(vampiresWithMoreThan500Victims);
+  
+      const vampiresWithUpTo150Victims = await Vampire.find({ victims: { $lte: 150 } });
+      console.log(vampiresWithUpTo150Victims);
+  
+      const vampiresNot210234Victims = await Vampire.find({ victims: { $ne: 210234 } });
+      console.log(vampiresNot210234Victims);
+  
+      const vampiresBetween150and500Victims = await Vampire.find({ 
+          victims: { $gt: 150, $lt: 500 } 
+      });
+      console.log(vampiresBetween150and500Victims);
+  
+      // This seems like a duplicate of the query above, so I'm just going to omit it
+      // However, if it's different, you can use the same async/await pattern
+  
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  // Call the function
+  fetchVampiresByComparison();
+  
 // by exists or does not exist
-Vampire.find({ title: { $exists: true } }, (err, vampiresWithTitle) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(vampiresWithTitle);
-  });
+async function fetchVampiresByExistence() {
+    try {
+      const vampiresWithTitle = await Vampire.find({ title: { $exists: true } });
+      console.log(vampiresWithTitle);
   
-  Vampire.find({ victims: { $exists: false } }, (err, vampiresWithoutVictims) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(vampiresWithoutVictims);
-  });
+      const vampiresWithoutVictims = await Vampire.find({ victims: { $exists: false } });
+      console.log(vampiresWithoutVictims);
   
-  Vampire.find({ 
-    title: { $exists: true }, 
-    victims: { $exists: false } 
-  }, (err, vampiresWithTitleAndNoVictims) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(vampiresWithTitleAndNoVictims);
-  });
+      const vampiresWithTitleAndNoVictims = await Vampire.find({ 
+        title: { $exists: true }, 
+        victims: { $exists: false } 
+      });
+      console.log(vampiresWithTitleAndNoVictims);
   
-  Vampire.find({ 
-    victims: { $exists: true, $gt: 1000 } 
-  }, (err, vampiresWithMoreThan1000Victims) => {
-    if (err) {
+      const vampiresWithMoreThan1000Victims = await Vampire.find({ 
+        victims: { $exists: true, $gt: 1000 } 
+      });
+      console.log(vampiresWithMoreThan1000Victims);
+  
+    } catch (err) {
       console.error(err);
-      return;
     }
-    console.log(vampiresWithMoreThan1000Victims);
-  });
+  }
+  
+  // Call the function
+  fetchVampiresByExistence();
+  
 //   select with or
 
-
-
+async function fetchVampires() {
+    try {
+      const vampiresFromSpecificLocations = await Vampire.find({
+        $or: [
+          { location: 'New York, New York, US' },
+          { location: 'New Orleans, Louisiana, US' }
+        ]
+      });
+      console.log(vampiresFromSpecificLocations);
+  
+      const broodingOrTragicVampires = await Vampire.find({
+        $or: [
+          { loves: 'brooding' },
+          { loves: 'being tragic' }
+        ]
+      });
+      console.log(broodingOrTragicVampires);
+  
+      const vampiresWithSpecificLikesOrVictims = await Vampire.find({
+        $or: [
+          { victims: { $gt: 1000 } },
+          { loves: 'marshmallows' }
+        ]
+      });
+      console.log(vampiresWithSpecificLikesOrVictims);
+  
+      const vampiresWithRedHairOrGreenEyes = await Vampire.find({
+        $or: [
+          { hair_color: 'red' },
+          { eye_color: 'green' }
+        ]
+      });
+      console.log(vampiresWithRedHairOrGreenEyes);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+ 
+  fetchVampires();
+  
 // Routes
 app.get('/vampires', async (req, res) => {
     try {
